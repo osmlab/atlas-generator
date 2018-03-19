49b027a47db36f76132bfce919d2e36b2e03eec6
matthieun <matthieu.nahoum@gmail.com>
2018-03-19 16:37:00 -0700

    Some test cleanup

--- src/test/java/org/openstreetmap/atlas/generator/PbfLocatorTest.java
+++ src/test/java/org/openstreetmap/atlas/generator/PbfLocatorTest.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.List;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.hadoop.fs.Path;
 import org.junit.Assert;
 import org.junit.Before;
@@ -16,8 +17,9 @@ import org.openstreetmap.atlas.geography.MultiPolygon;
 import org.openstreetmap.atlas.geography.Polygon;
 import org.openstreetmap.atlas.geography.Rectangle;
 import org.openstreetmap.atlas.geography.sharding.SlippyTileSharding;
-import org.openstreetmap.atlas.streaming.Streams;
 import org.openstreetmap.atlas.utilities.collections.Iterables;
+
+import com.amazonaws.util.StringInputStream;
 
 /**
  * @author matthieun
@@ -25,22 +27,39 @@ import org.openstreetmap.atlas.utilities.collections.Iterables;
 public class PbfLocatorTest
 {
     private static final String version = "07082015";
-    private static final String fileName = "BHS_" + version + ".pbf";
 
     @Before
     public void init()
     {
+        uploadFile(10 + "/" + 291 + "-" + 438 + ".osm.pbf");
+        uploadFile(10 + "/" + 292 + "-" + 438 + ".osm.pbf");
         uploadFile(10 + "-" + 291 + "-" + 438 + ".pbf");
         uploadFile(10 + "-" + 292 + "-" + 438 + ".pbf");
         System.out.println(ResourceFileSystem.files());
     }
 
     @Test
-    public void testTileFinder()
+    public void testDefaultTileFinder()
     {
-        final PbfLocator locator = new PbfLocator("resource://" + version,
-                PbfLocator.DEFAULT_SCHEME, new SlippyTileSharding(10),
-                ResourceFileSystem.simpleconfiguration());
+        // Test a default scheme
+        final String scheme = PbfLocator.DEFAULT_SCHEME;
+        final PbfLocator locator = new PbfLocator("resource://" + version, scheme,
+                new SlippyTileSharding(10), ResourceFileSystem.simpleconfiguration());
+        final Polygon outer = Rectangle.forCorners(Location.forString("24.953302,-77.608195"),
+                Location.forString("25.131896,-77.207033"));
+        final MultiPolygon multiPolygon = MultiPolygon.forPolygon(outer);
+        final List<LocatedPbf> tiles = Iterables.asList(locator.pbfsCovering(multiPolygon));
+        Assert.assertEquals(2, tiles.size());
+    }
+
+    @Test
+    public void testDifferentTileFinder()
+    {
+        // Test a non-default scheme
+        final String scheme = PbfLocator.ZOOM + "/" + PbfLocator.X_INDEX + "-" + PbfLocator.Y_INDEX
+                + ".osm.pbf";
+        final PbfLocator locator = new PbfLocator("resource://" + version, scheme,
+                new SlippyTileSharding(10), ResourceFileSystem.simpleconfiguration());
         final Polygon outer = Rectangle.forCorners(Location.forString("24.953302,-77.608195"),
                 Location.forString("25.131896,-77.207033"));
         final MultiPolygon multiPolygon = MultiPolygon.forPolygon(outer);
@@ -50,19 +69,12 @@ public class PbfLocatorTest
 
     private void uploadFile(final String name)
     {
-        try
+        try (ResourceFileSystem resourceFileSystem = new ResourceFileSystem();
+                OutputStream output = resourceFileSystem
+                        .create(new Path("resource://" + version + "/" + name));
+                InputStream input = new StringInputStream("test"))
         {
-            @SuppressWarnings("resource")
-            final OutputStream output = new ResourceFileSystem()
-                    .create(new Path("resource://" + version + "/" + name));
-            final InputStream input = PbfLocatorTest.class.getResourceAsStream(fileName);
-            int index = -1;
-            while ((index = input.read()) >= 0)
-            {
-                output.write(index);
-            }
-            Streams.close(input);
-            Streams.close(output);
+            IOUtils.copy(input, output);
         }
         catch (final Exception e)
         {
