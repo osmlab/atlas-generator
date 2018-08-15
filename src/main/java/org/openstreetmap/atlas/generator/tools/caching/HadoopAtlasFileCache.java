c8c175389ca7e0d3f4bd4f494c845a661e157a5e
lucaspcram <lucaspcram@gmail.com>
2018-08-15 15:23:47 -0700

    Added basic HadoopAtlasFileCache functionality

--- src/main/java/org/openstreetmap/atlas/generator/tools/caching/HadoopAtlasFileCache.java
+++ src/main/java/org/openstreetmap/atlas/generator/tools/caching/HadoopAtlasFileCache.java
@@ -1,26 +1,70 @@
 package org.openstreetmap.atlas.generator.tools.caching;
 
 import java.net.URI;
-import java.util.function.Function;
+import java.net.URISyntaxException;
+import java.util.Map;
+import java.util.Optional;
 
+import org.openstreetmap.atlas.exception.CoreException;
+import org.openstreetmap.atlas.generator.tools.filesystem.FileSystemHelper;
+import org.openstreetmap.atlas.geography.sharding.Shard;
+import org.openstreetmap.atlas.streaming.resource.FileSuffix;
 import org.openstreetmap.atlas.streaming.resource.Resource;
 import org.openstreetmap.atlas.utilities.caching.ConcurrentResourceCache;
-import org.openstreetmap.atlas.utilities.caching.strategies.CachingStrategy;
+import org.openstreetmap.atlas.utilities.caching.strategies.SystemTemporaryFileCachingStrategy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Cache an atlas file stored in the standard way (parentpath/COUNTRY/COUNTRY_z-x-y.atlas) to a
  * system temporary location. This cache is designed to be used with atlases that can be fetched
- * from a hadoop file system.
+ * from a hadoop file system. This cache is threadsafe.
  *
  * @author lcram
  */
 public class HadoopAtlasFileCache extends ConcurrentResourceCache
 {
+    /**
+     * This field can be static since there is only 1 global filesystem! The advantage to this
+     * approach is that all instances of {@link HadoopAtlasFileCache} will share the same underlying
+     * file cache, and so cross-cache hits are possible (and desired!).
+     */
+    private static final SystemTemporaryFileCachingStrategy GLOBAL_STRATEGY = new SystemTemporaryFileCachingStrategy();
+    private static final Logger logger = LoggerFactory.getLogger(HadoopAtlasFileCache.class);
 
-    public HadoopAtlasFileCache(final CachingStrategy cachingStrategy,
-            final Function<URI, Resource> fetcher)
+    private final String parentAtlasPath;
+
+    /**
+     * Create a new cache.
+     *
+     * @param parentAtlasPath
+     *            The parent path to the atlas files. This might look like hdfs://some/path/to/files
+     * @param configuration
+     *            The configuration map
+     */
+    public HadoopAtlasFileCache(final String parentAtlasPath,
+            final Map<String, String> configuration)
     {
-        super(cachingStrategy, fetcher);
-        // TODO Auto-generated constructor stub
+        super(GLOBAL_STRATEGY, uri -> FileSystemHelper.resource(uri.toString(), configuration));
+        this.parentAtlasPath = parentAtlasPath;
+    }
+
+    public Optional<Resource> get(final String country, final Shard shard)
+    {
+        final String atlasName = String.format("%s_%s", country, shard.getName());
+        final String atlasURIString = this.parentAtlasPath + "/" + country + "/" + atlasName
+                + FileSuffix.ATLAS.toString();
+        final URI atlasURI;
+
+        try
+        {
+            atlasURI = new URI(atlasURIString);
+        }
+        catch (final URISyntaxException exception)
+        {
+            throw new CoreException("Bad URI syntax: {}", atlasURIString, exception);
+        }
+
+        return this.get(atlasURI);
     }
 }
