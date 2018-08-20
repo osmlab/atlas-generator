b4e9f69925ec0f863bad2ef3597d8bf9103396d3
lucaspcram <lucaspcram@gmail.com>
2018-08-20 08:56:19 -0700

    Added a HadoopAtlasFileCache (#48)
    * Bumped atlas dependency up to 5.1.8 to pull in new caching code
    
    * Started on atlas caching
    
    * Added basic HadoopAtlasFileCache functionality
    
    * Added note about bad synch code
    
    * Fixed synchronization issues by removing global state
    
    * Changed cache to return atlases. This will not work until resource loader changes in atlas are merged
    
    * Changed cacher back to resource based
    
    * Added a unit test
    
    * Added comment to test
    
    * Updated test delete

---
+++ src/main/java/org/openstreetmap/atlas/generator/tools/caching/HadoopAtlasFileCache.java
@@ -0,0 +1,74 @@
+package org.openstreetmap.atlas.generator.tools.caching;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Map;
+import java.util.Optional;
+
+import org.openstreetmap.atlas.exception.CoreException;
+import org.openstreetmap.atlas.generator.tools.filesystem.FileSystemHelper;
+import org.openstreetmap.atlas.geography.sharding.Shard;
+import org.openstreetmap.atlas.streaming.resource.FileSuffix;
+import org.openstreetmap.atlas.streaming.resource.Resource;
+import org.openstreetmap.atlas.utilities.caching.ConcurrentResourceCache;
+import org.openstreetmap.atlas.utilities.caching.strategies.SystemTemporaryFileCachingStrategy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Cache an atlas file stored in the standard way (parentpath/COUNTRY/COUNTRY_z-x-y.atlas) to a
+ * system temporary location. This cache is designed to be used with atlases that can be fetched
+ * from a hadoop file system. This cache is threadsafe.
+ *
+ * @author lcram
+ */
+public class HadoopAtlasFileCache extends ConcurrentResourceCache
+{
+    private static final Logger logger = LoggerFactory.getLogger(HadoopAtlasFileCache.class);
+
+    private final String parentAtlasPath;
+
+    /**
+     * Create a new cache.
+     *
+     * @param parentAtlasPath
+     *            The parent path to the atlas files. This might look like hdfs://some/path/to/files
+     * @param configuration
+     *            The configuration map
+     */
+    public HadoopAtlasFileCache(final String parentAtlasPath,
+            final Map<String, String> configuration)
+    {
+        super(new SystemTemporaryFileCachingStrategy(),
+                uri -> FileSystemHelper.resource(uri.toString(), configuration));
+        this.parentAtlasPath = parentAtlasPath;
+    }
+
+    /**
+     * Get an {@link Optional} of an atlas resource specified by the given parameters.
+     *
+     * @param country
+     *            The ISO country code of the desired shard
+     * @param shard
+     *            The {@link Shard} object representing the shard
+     * @return an {@link Optional} wrapping the shard
+     */
+    public Optional<Resource> get(final String country, final Shard shard)
+    {
+        final String atlasName = String.format("%s_%s", country, shard.getName());
+        final String atlasURIString = this.parentAtlasPath + "/" + country + "/" + atlasName
+                + FileSuffix.ATLAS.toString();
+        final URI atlasURI;
+
+        try
+        {
+            atlasURI = new URI(atlasURIString);
+        }
+        catch (final URISyntaxException exception)
+        {
+            throw new CoreException("Bad URI syntax: {}", atlasURIString, exception);
+        }
+
+        return this.get(atlasURI);
+    }
+}
---
+++ src/test/java/org/openstreetmap/atlas/generator/tools/caching/HadoopAtlasFileCacheTest.java
@@ -0,0 +1,56 @@
+package org.openstreetmap.atlas.generator.tools.caching;
+
+import java.util.HashMap;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openstreetmap.atlas.geography.sharding.SlippyTile;
+import org.openstreetmap.atlas.streaming.resource.File;
+import org.openstreetmap.atlas.streaming.resource.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author lcram
+ */
+public class HadoopAtlasFileCacheTest
+{
+    private static final Logger logger = LoggerFactory.getLogger(HadoopAtlasFileCacheTest.class);
+
+    @Test
+    public void testCache()
+    {
+        final File parent = File.temporaryFolder();
+        final File parentAtlas = new File(parent + "/atlas");
+        final File parentAtlasCountry = new File(parentAtlas + "/AAA");
+        parentAtlasCountry.mkdirs();
+        try
+        {
+            final File atlas1 = parentAtlasCountry.child("AAA_1-1-1.atlas");
+            atlas1.writeAndClose("1");
+            final File atlas2 = parentAtlasCountry.child("AAA_2-2-2.atlas");
+            atlas2.writeAndClose("2");
+
+            final String path = "file://" + parentAtlas.toString();
+            final HadoopAtlasFileCache cache = new HadoopAtlasFileCache(path, new HashMap<>());
+
+            // cache miss, this will create the cached copy
+            final Resource resource1 = cache.get("AAA", new SlippyTile(1, 1, 1)).get();
+            final Resource resource2 = cache.get("AAA", new SlippyTile(2, 2, 2)).get();
+
+            Assert.assertEquals("1", resource1.firstLine());
+            Assert.assertEquals("2", resource2.firstLine());
+
+            // cache hit, using cached copy
+            final Resource resource3 = cache.get("AAA", new SlippyTile(1, 1, 1)).get();
+            final Resource resource4 = cache.get("AAA", new SlippyTile(2, 2, 2)).get();
+
+            Assert.assertEquals("1", resource3.firstLine());
+            Assert.assertEquals("2", resource4.firstLine());
+        }
+        finally
+        {
+            parent.deleteRecursively();
+        }
+    }
+}
