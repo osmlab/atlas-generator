ec97fb4087918516de79d19a580e867f74ea80df
lucaspcram <lucaspcram@gmail.com>
2018-08-22 13:05:36 -0700

    Spotless saveaction discrepancy strikes again.

--- src/main/java/org/openstreetmap/atlas/generator/AtlasGenerator.java
+++ src/main/java/org/openstreetmap/atlas/generator/AtlasGenerator.java
@@ -269,7 +269,8 @@ public class AtlasGenerator extends SparkJob
         final String shouldAlwaysSliceConfiguration = (String) command
                 .get(SHOULD_ALWAYS_SLICE_CONFIGURATION);
         final Predicate<Taggable> shouldAlwaysSlicePredicate = shouldAlwaysSliceConfiguration == null
-                ? taggable -> false : AtlasGeneratorHelper.getTaggableFilterFrom(
+                ? taggable -> false
+                : AtlasGeneratorHelper.getTaggableFilterFrom(
                         FileSystemHelper.resource(shouldAlwaysSliceConfiguration, sparkContext));
         final String output = output(command);
         final boolean useJavaFormat = (boolean) command.get(USE_JAVA_FORMAT);
