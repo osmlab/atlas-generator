7193996507d10b8ba28a1d49e22d89a0ad57effc
James Gage <me@jamesgage.com>
2018-10-11 11:48:42 -0700

    added a default boundary map and configuration options

--- src/main/java/org/openstreetmap/atlas/generator/world/WorldAtlasGenerator.java
+++ src/main/java/org/openstreetmap/atlas/generator/world/WorldAtlasGenerator.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.openstreetmap.atlas.generator.AtlasGeneratorHelper;
 import org.openstreetmap.atlas.geography.MultiPolygon;
+import org.openstreetmap.atlas.geography.Rectangle;
 import org.openstreetmap.atlas.geography.atlas.Atlas;
 import org.openstreetmap.atlas.geography.atlas.AtlasMetaData;
 import org.openstreetmap.atlas.geography.atlas.multi.MultiAtlas;
@@ -19,8 +20,11 @@ import org.openstreetmap.atlas.geography.sharding.Shard;
 import org.openstreetmap.atlas.geography.sharding.SlippyTile;
 import org.openstreetmap.atlas.streaming.resource.File;
 import org.openstreetmap.atlas.streaming.resource.FileSuffix;
+import org.openstreetmap.atlas.streaming.resource.StringResource;
 import org.openstreetmap.atlas.tags.Taggable;
+import org.openstreetmap.atlas.tags.filters.ConfiguredTaggableFilter;
 import org.openstreetmap.atlas.utilities.collections.Maps;
+import org.openstreetmap.atlas.utilities.configuration.StandardConfiguration;
 import org.openstreetmap.atlas.utilities.conversion.StringConverter;
 import org.openstreetmap.atlas.utilities.runtime.Command;
 import org.openstreetmap.atlas.utilities.runtime.CommandMap;
@@ -45,7 +49,7 @@ public class WorldAtlasGenerator extends Command
             "The file that will contain the statistics", File::new, Optionality.OPTIONAL);
     public static final Switch<CountryBoundaryMap> BOUNDARIES = new Switch<>("boundaries",
             "The boundary map to use", value -> CountryBoundaryMap.fromPlainText(new File(value)),
-            Optionality.REQUIRED);
+            Optionality.OPTIONAL);
     public static final Switch<String> CODE_VERSION = new Switch<>("codeVersion",
             "The code version", StringConverter.IDENTITY, Optionality.OPTIONAL, "unknown");
     public static final Switch<String> DATA_VERSION = new Switch<>("dataVersion",
@@ -59,6 +63,20 @@ public class WorldAtlasGenerator extends Command
                     + " always be attempted regardless of the number of countries it intersects according to the"
                     + " country boundary map's grid index.",
             StringConverter.IDENTITY, Optionality.OPTIONAL);
+    public static final Switch<File> PBF_WAY_CONFIGURATION = new Switch<>("osmPbfWayConfiguration",
+            "The path to the configuration file that defines which PBF Ways becomes an Atlas Entity.",
+            File::new, Optionality.OPTIONAL);
+    public static final Switch<File> PBF_NODE_CONFIGURATION = new Switch<>(
+            "osmPbfNodeConfiguration",
+            "The path to the configuration file that defines which PBF Nodes becomes an Atlas Entity.",
+            File::new, Optionality.OPTIONAL);
+    public static final Switch<File> PBF_RELATION_CONFIGURATION = new Switch<>(
+            "osmPbfRelationConfiguration",
+            "The path to the configuration file that defines which PBF Relations becomes an Atlas Entity",
+            File::new, Optionality.OPTIONAL);
+    // filter that does no filtering
+    private static final ConfiguredTaggableFilter PBF_NO_FILTER_CONFIGURATION = new ConfiguredTaggableFilter(
+            new StandardConfiguration(new StringResource("{\n    \"filters\": [\n\n    ]\n}")));
 
     public static void main(final String[] args)
     {
@@ -68,7 +86,16 @@ public class WorldAtlasGenerator extends Command
     @Override
     protected int onRun(final CommandMap command)
     {
-        final CountryBoundaryMap countryBoundaryMap = (CountryBoundaryMap) command.get(BOUNDARIES);
+        CountryBoundaryMap countryBoundaryMap = (CountryBoundaryMap) command.get(BOUNDARIES);
+        if (countryBoundaryMap == null)
+        {
+            final StringResource wholeWorld = new StringResource(
+                    "AAA||" + Rectangle.MAXIMUM.toWkt());
+            countryBoundaryMap = CountryBoundaryMap.fromPlainText(wholeWorld);
+        }
+        final File pbfWayConfiguration = (File) command.get(PBF_WAY_CONFIGURATION);
+        final File pbfNodeConfiguration = (File) command.get(PBF_NODE_CONFIGURATION);
+        final File pbfRelationConfiguration = (File) command.get(PBF_RELATION_CONFIGURATION);
         final File pbf = (File) command.get(PBF);
         final File output = (File) command.get(ATLAS);
         final File statisticsOutput = (File) command.get(STATISTICS);
@@ -88,6 +115,33 @@ public class WorldAtlasGenerator extends Command
         final AtlasLoadingOption loadingOptions = AtlasLoadingOption
                 .createOptionWithAllEnabled(countryBoundaryMap)
                 .setAdditionalCountryCodes(countryBoundaryMap.allCountryNames());
+        if (pbfWayConfiguration == null)
+        {
+            loadingOptions.setOsmPbfWayFilter(PBF_NO_FILTER_CONFIGURATION);
+        }
+        else
+        {
+            loadingOptions.setOsmPbfWayFilter(
+                    new ConfiguredTaggableFilter(new StandardConfiguration(pbfWayConfiguration)));
+        }
+        if (pbfNodeConfiguration == null)
+        {
+            loadingOptions.setOsmPbfNodeFilter(PBF_NO_FILTER_CONFIGURATION);
+        }
+        else
+        {
+            loadingOptions.setOsmPbfNodeFilter(
+                    new ConfiguredTaggableFilter(new StandardConfiguration(pbfNodeConfiguration)));
+        }
+        if (pbfRelationConfiguration == null)
+        {
+            loadingOptions.setOsmPbfRelationFilter(PBF_NO_FILTER_CONFIGURATION);
+        }
+        else
+        {
+            loadingOptions.setOsmPbfRelationFilter(new ConfiguredTaggableFilter(
+                    new StandardConfiguration(pbfRelationConfiguration)));
+        }
         final AtlasMetaData metaData = new AtlasMetaData(null, true, codeVersion, dataVersion,
                 "WORLD", world.getName(), Maps.hashMap());
 
@@ -126,10 +180,13 @@ public class WorldAtlasGenerator extends Command
 
         logger.info("Saving world atlas to {}", output);
         atlas.save(output);
-        logger.info("Generating world statistics...");
-        final AtlasStatistics statistics = counter.processAtlas(atlas);
-        logger.info("Saving world statistics to {}", statisticsOutput);
-        statistics.save(statisticsOutput);
+        if (statisticsOutput != null)
+        {
+            logger.info("Generating world statistics...");
+            final AtlasStatistics statistics = counter.processAtlas(atlas);
+            logger.info("Saving world statistics to {}", statisticsOutput);
+            statistics.save(statisticsOutput);
+        }
 
         logger.info("Finished creating world atlas in {}", start.elapsedSince());
 
@@ -140,7 +197,8 @@ public class WorldAtlasGenerator extends Command
     protected SwitchList switches()
     {
         return new SwitchList().with(PBF, ATLAS, STATISTICS, BOUNDARIES, CODE_VERSION, DATA_VERSION,
-                SHOULD_ALWAYS_SLICE_CONFIGURATION);
+                SHOULD_ALWAYS_SLICE_CONFIGURATION, PBF_WAY_CONFIGURATION, PBF_NODE_CONFIGURATION,
+                PBF_RELATION_CONFIGURATION);
     }
 
 }
