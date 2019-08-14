e098549ebfb18be35b6640d835802c3f913c87e6
matthieun <matthieu.nahoum@gmail.com>
2019-08-14 16:10:46 -0700

    Support for atlas sharding

---
+++ src/test/java/org/openstreetmap/atlas/generator/sharding/AtlasShardingTest.java
@@ -0,0 +1,23 @@
+package org.openstreetmap.atlas.generator.sharding;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openstreetmap.atlas.generator.tools.streaming.ResourceFileSystem;
+import org.openstreetmap.atlas.geography.sharding.GeoHashSharding;
+import org.openstreetmap.atlas.geography.sharding.Sharding;
+
+/**
+ * @author matthieun
+ */
+public class AtlasShardingTest
+{
+    @Test
+    public void testGeoHashSharding()
+    {
+        final Sharding sharding = AtlasSharding.forString("geohash@7",
+                ResourceFileSystem.simpleconfiguration());
+        Assert.assertTrue(sharding instanceof GeoHashSharding);
+        final GeoHashSharding geoHashSharding = (GeoHashSharding) sharding;
+        Assert.assertEquals(7, geoHashSharding.getPrecision());
+    }
+}
